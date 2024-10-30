78cb8870a405391ae0e6c4b22f37af91b8ea842f
CarlosHernandez14 <carloskvpchc@gmail.com>
2024-10-30 10:31:24 -0600

    Agregue pantalla home influencer

--- DonacionesCreadores/Properties/Resources.Designer.cs
+++ DonacionesCreadores/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace DonacionesCreadores.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_publicaciones {
+            get {
+                object obj = ResourceManager.GetObject("icon-publicaciones", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Influencer_Marketing {
             get {
                 object obj = ResourceManager.GetObject("Influencer Marketing", resourceCulture);
